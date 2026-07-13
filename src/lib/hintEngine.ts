3e11c823f6a79decd165ece355ea965fbd30bf75
Almnjshy <115098081+Almnjshy@users.noreply.github.com>
2026-07-13 23:21:14 +0300

    Delete hintEngine.ts

--- src/lib/hintEngine.ts
+++
@@ -1,3 +0,0 @@
-export function getBestMove(state: any, playerIndex: number) { return null; }
-export function getHintMessage(state: any, playerIndex: number) { return ''; }
-export function shouldDraw(state: any, playerIndex: number) { return false; }
